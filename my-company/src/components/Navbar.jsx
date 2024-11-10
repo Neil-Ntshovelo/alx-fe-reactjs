f7d4a01c9e89d8ceb53cbdafa643cee92dc7412c
Ntshovelo Nell <neilmunetwa@gmail.com>
2024-11-11 00:38:24 +0200

    Add files via upload

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,11 +2,17 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', background: '#333', color: '#fff' }}>
-      <Link to="/" style={{ color: '#fff', margin: '0 10px' }}>Home</Link>
-      <Link to="/about" style={{ color: '#fff', margin: '0 10px' }}>About</Link>
-      <Link to="/services" style={{ color: '#fff', margin: '0 10px' }}>Services</Link>
-      <Link to="/contact" style={{ color: '#fff', margin: '0 10px' }}>Contact</Link>
+    <nav style={{ 
+      padding: '10px', 
+      backgroundColor: '#333', 
+      color: '#fff', 
+      display: 'flex', 
+      justifyContent: 'space-around' 
+    }}>
+      <Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>Home</Link>
+      <Link to="/about" style={{ color: '#fff', textDecoration: 'none' }}>About</Link>
+      <Link to="/services" style={{ color: '#fff', textDecoration: 'none' }}>Services</Link>
+      <Link to="/contact" style={{ color: '#fff', textDecoration: 'none' }}>Contact</Link>
     </nav>
   );
 }
