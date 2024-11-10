d17aea216fb4f0f1ddf8dd050314283eb9f49159
Ntshovelo Nell <neilmunetwa@gmail.com>
2024-11-10 18:36:45 +0200

    Add files via upload

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,37 +1,17 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import React from 'react';
-import './App.css'
-import './index.css'
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile/UserProfile'
-
+import ProfilePage from './components/ProfilePage';
+import UserContext from './components/UserContext';
 
 
 
 function App() {
-  
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    
-    <>
-      <WelcomeMessage />
-
-      <Header />
-    
-      <MainContent />  
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <UserProfile name="Ntshovelo" age="23" bio=" I am a programmer who specialise in Front-end development and this is my React progress " />
-      <Footer />
-    
-     
-    </>
-     
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
-export default App
+export default App;
