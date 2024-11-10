8f0acffa8f2cded9da3b827cba04d9845beb6493
Ntshovelo Nell <neilmunetwa@gmail.com>
2024-11-10 13:41:38 +0200

    Add files via upload

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,6 +1,11 @@
 const Footer =() =>{
     return(
-        <footer>
+        <footer  style={{ 
+            backgroundColor: 'grey', 
+            color: '#fff', padding: '10px', 
+            textAlign: 'center', 
+            fontFamily: 'Arial, sans-serif' , 
+            borderRadius: '4px',}}>
             <p>© 2023 City Lovers</p>
         </footer> 
     );
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent(){
     return(
-        <main>
+        <main style={{ padding: '20px', textAlign: 'center', fontFamily: 'Arial, sans-serif' }}>
             <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
