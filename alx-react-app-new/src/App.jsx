b052ce31c8cf137e5ad647dd64df4a7750733f7b
Ntshovelo Nell <neilmunetwa@gmail.com>
2024-11-10 14:08:25 +0200

    Add files via upload

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,8 +7,10 @@ import './index.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
+import Counter from './components/Counter';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile/UserProfile'
+
 
 
 
@@ -23,7 +25,8 @@ function App() {
 
       <Header />
     
-      <MainContent />  
+      <MainContent /> 
+      <Counter />  
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <UserProfile name="Ntshovelo" age="23" bio=" I am a programmer who specialise in Front-end development and this is my React progress " />
       <Footer />
