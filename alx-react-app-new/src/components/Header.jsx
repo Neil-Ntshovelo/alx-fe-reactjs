24bdf55e766e262c61f476088eb146d3e27b382f
Ntshovelo Nell <neilmunetwa@gmail.com>
2024-11-10 13:00:36 +0200

    Add files via upload

---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+
+function Header(){
+    return(
+        <div>
+            <header>
+                <h1>My Favorite Cities</h1>
+            </header>
+        </div>
+        
+        
+        
+    );
+}
+
+export default Header;
