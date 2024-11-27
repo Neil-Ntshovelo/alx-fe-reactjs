141e17ebc6136963701e2dfdec4b8dd0acdd84f9
Ntshovelo Nell <neilmunetwa@gmail.com>
2024-11-27 17:48:35 +0200

    Add files via upload

--- react-router-advanced/src/pages/Home.jsx
+++ react-router-advanced/src/pages/Home.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+const Home = () => {
+  return (
+    <div>
+      <h1 className="home-title">Welcome to the Home Page</h1>
+      <p className="home-description">
+        Please <Link to="/profile/123" aria-label="View Profile 123">click here</Link> to view the profile page.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
