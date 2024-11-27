f0cce3e865d843f3705a387c0bb0c206b928299b
Ntshovelo Nell <neilmunetwa@gmail.com>
2024-11-27 17:45:58 +0200

    Add files via upload

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,1 +1,24 @@
+import React from 'react';
+import { Routes, Route, Link, useParams } from 'react-router-dom';
+import ProfileDetails from './ProfileDetails';
+import ProfileSettings from './ProfileSettings';
 
+const Profile = () => {
+  const { userId } = useParams(); // Get userId from the URL
+
+  return (
+    <div>
+      <h1>Profile</h1>
+      <nav>
+        <Link to="details">Details</Link>
+        <Link to="settings">Settings</Link>
+      </nav>
+      <Routes>
+        <Route path="details" element={<ProfileDetails userId={userId} />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Profile;
---
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProfileDetails = ({ userId }) => {
+  return <div>User Profile Details for User ID: {userId}</div>;
+};
+
+export default ProfileDetails;
