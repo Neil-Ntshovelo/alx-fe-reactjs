8966cf6f4ddaf85f424738308453c122f2d5bde1
Ntshovelo Nell <neilmunetwa@gmail.com>
2024-11-28 07:08:29 +0200

    Add files via upload

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,1 +1,15 @@
 
+import React from 'react';
+import TodoList from './components/TodoList.jsx';
+
+
+function App() {
+    return (
+        <div className="App">
+            <TodoList />
+            
+        </div>
+    );
+}
+
+export default App;
