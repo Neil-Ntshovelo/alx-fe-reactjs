36bc5c3337ea62412eff4a55a7c64c30cdb491d4
Ntshovelo Nell <neilmunetwa@gmail.com>
2024-12-01 08:58:50 +0200

    Add files via upload

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 
 function UserProfile({ name, description }) {
   return (
-    <div className="user-profile bg-gray-100 sm:p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm md:max-w-md mx-auto my-20 rounded-lg shadow-lg">
       <img 
         src="https://via.placeholder.com/150"
         alt={`${name}'s profile`} 
-        className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto" 
+        className="rounded-full w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 mx-auto" 
       />
-      <h1 className="text-lg sm:text-xl text-blue-800 my-4">{name}</h1>
-      <p className="text-sm sm:text-base text-gray-600">{description}</p>
+      <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4">{name}</h1>
+      <p className="text-sm sm:text-base md:text-lg text-gray-600">{description}</p>
     </div>
   );
 }
