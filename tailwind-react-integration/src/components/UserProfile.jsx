bea30c097b0cf1a54a412bc5c4a763ab462686ab
Ntshovelo Nell <neilmunetwa@gmail.com>
2024-12-01 08:47:17 +0200

    Add files via upload

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function UserProfile({ name , description }) {
+function UserProfile({ name, description }) {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img 
         src="https://via.placeholder.com/150"
         alt={`${name}'s profile`} 
-        className="rounded-full w-36 h-36 mx-auto" 
+        className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto" 
       />
-      <h1 className="text-xl text-blue-800 my-4">{name}</h1>
-      <p className="text-gray-600 text-base">{description}</p>
+      <h1 className="text-lg sm:text-xl text-blue-800 my-4">{name}</h1>
+      <p className="text-sm sm:text-base text-gray-600">{description}</p>
     </div>
   );
 }
 
 UserProfile.propTypes = {
   name: PropTypes.string.isRequired,
-  imageUrl: PropTypes.string,
-  description : PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
 };
-UserProfile.propTypes = {
-
-}
-
 
 export default UserProfile;
