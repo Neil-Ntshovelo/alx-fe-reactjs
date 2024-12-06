07be1596b5747a75d24ff6e47d99b6b501c06179
Ntshovelo Nell <neilmunetwa@gmail.com>
2024-12-06 18:08:19 +0200

    Add files via upload

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,13 +1,24 @@
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import RecipeDetails from './components/RecipeDetails';
+import RecipeDetail from './components/RecipeDetail';
+import AddRecipeForm from './components/AddRecipeForm';
+import { useState } from 'react';
+import recipesData from './data.json'; 
 
 const App = () => {
+  const [recipes, setRecipes] = useState(recipesData);
+
+  const handleAddRecipe = (newRecipe) => {
+    setRecipes((prevRecipes) => [...prevRecipes, newRecipe]);
+  };
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/RecipeDetails/:id" element={<RecipeDetails />} />
+        <Route path="/" element={<HomePage recipes={recipes} />} />
+        <Route path="/RecipeDetails/:id" element={<RecipeDetail recipes={recipes} />} />
+        <Route path="/add-recipe" element={<AddRecipeForm onAddRecipe={handleAddRecipe} />} />
       </Routes>
     </Router>
   );
