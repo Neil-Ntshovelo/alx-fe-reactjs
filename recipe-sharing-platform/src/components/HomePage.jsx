2b8a4de7717835a5a766c3a85379a00a91542ce0
Ntshovelo Nell <neilmunetwa@gmail.com>
2024-12-06 23:42:21 +0200

    Add files via upload

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'; 
+import data from '../data.json';
 
 const HomePage = ({ recipes }) => {
   const [loading, setLoading] = useState(false);
