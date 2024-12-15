eae43504fa705ac6c71fa10ac53af60a9ffa0289
Ntshovelo Nell <neilmunetwa@gmail.com>
2024-12-15 18:29:08 +0200

    Add files via upload

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-
+const API_URL = 'https://api.github.com/users';
 
 export const fetchUser  = async (username) => {
     try {
-        const response = await axios.get(`https://api.github.com/users/${username}`, {
+        const response = await axios.get(`${API_URL}/${username}`, {
             headers: {
                 Authorization: `token ${process.env.REACT_APP_GITHUB_API_KEY}`, 
             },
