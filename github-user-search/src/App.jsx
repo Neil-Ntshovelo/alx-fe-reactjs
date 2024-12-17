87a46c43a1cf50f9d8e39c02b62dba4536a7c1a0
Ntshovelo Nell <neilmunetwa@gmail.com>
2024-12-17 13:55:32 +0200

    Add files via upload

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Search from './components/Search';
 
-
-
-
 const App = () => {
     return (
-        <div>
-            <h1>GitHub User Search</h1>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
+            <h1 className="text-3xl font-bold mb-6 text-center text-blue-900">
+                GitHub User Search
+            </h1>
             <Search />
         </div>
     );
