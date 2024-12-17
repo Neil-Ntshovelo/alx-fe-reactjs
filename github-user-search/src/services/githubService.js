de7db82ca498096ec00d4d1196316617f8783b62
Ntshovelo Nell <neilmunetwa@gmail.com>
2024-12-17 13:52:42 +0200

    Add files via upload

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,13 +1,18 @@
-
 import axios from 'axios';
 
-const fetchUserData = async (username) => { 
+const fetchUserData = async (username, location, minRepos) => {
     try {
-        const response = await axios.get(`https://api.github.com/users/${username}`);
-        return response.data;
+       
+        let query = `type:user`;
+        if (username) query += `+${username}`;
+        if (location) query += `+location:${location}`;
+        if (minRepos) query += `+repos:>=${minRepos}`;
+
+        const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
+        return response.data; 
     } catch (error) {
         throw new Error('User  not found');
     }
 };
 
-export { fetchUserData }; 
+export { fetchUserData };
